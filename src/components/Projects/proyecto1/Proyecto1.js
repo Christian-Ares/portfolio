23e4d7dce8dbe04a9c812c7e47cf33fc6e422fc0
Christian Ares <christianaresvillan@gmail.com>
2021-01-23 21:24:06 +0100

    Buttons partially working

--- src/components/Projects/proyecto1/Proyecto1.js
+++ src/components/Projects/proyecto1/Proyecto1.js
@@ -1,19 +1,23 @@
-// import { Redirect } from "react-router-dom";
 import { Button } from '@material-ui/core'
 import React from 'react'
+// import { Redirect } from 'react-router-dom'
 
 export const Proyecto1 = () => {
 
-   const handleClick = (e) => {
-        e.preventDefault();
-        console.log('Botton clicado');
-    };
+    function ProjectDemo() {
+        // <Redirect to="https://christian-ares.github.io/The-Legend-of-Ironhacker/" />
+        console.log('Boton Clicado')
+        }
+
+    function ProjectCode() {
+        // <Redirect to="https://github.com/Christian-Ares/The-Legend-of-Ironhacker" />
+        console.log('Boton Clicado')
+        }
 
     return (
         <div>
-           <button onClick={()=> this.handleClick} >Demo</button>
-           {/* <button><a href="https://github.com/Christian-Ares/The-Legend-of-Ironhacker" ></a>Source Code</button> */}
-           <Button onClick={()=> this.handleClick} variant="contained" color="primary">Demo</Button>
+           <Button onClick={ProjectDemo} variant="contained" color="primary">Demo</Button>
+           <Button onClick={ProjectCode} variant="contained" color="primary">Source Code</Button>
         </div>
     )
 }
