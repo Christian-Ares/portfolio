129bb713775727baaa2202d232e142d6c3f8da12
Christian Ares <christianaresvillan@gmail.com>
2021-01-23 21:28:56 +0100

    Buttons on projects partially working

--- src/components/Projects/proyecto2/Proyecto2.js
+++ src/components/Projects/proyecto2/Proyecto2.js
@@ -1,10 +1,22 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 
 export const Proyecto2 = () => {
+
+    function ProjectDemo() {
+        // <Redirect to="https://mascotapp-v1.herokuapp.com/" />
+        console.log('Boton Clicado')
+        }
+
+    function ProjectCode() {
+        // <Redirect to="https://github.com/Christian-Ares/MascotApp" />
+        console.log('Boton Clicado')
+        }
+
     return (
         <div>
-            <button>Proyecto 2</button>
-            <button>Proyecto 2</button>
+            <Button onClick={ProjectDemo} variant="contained" color="primary">Demo</Button>
+            <Button onClick={ProjectCode} variant="contained" color="primary">Source Code</Button>
         </div>
     )
 }
--- src/components/Projects/proyecto3/Proyecto3.js
+++ src/components/Projects/proyecto3/Proyecto3.js
@@ -2,10 +2,22 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 
 export const Proyecto3 = () => {
+
+    function ProjectDemo() {
+        // <Redirect to="https://escuela-infantil.netlify.app/" />
+        console.log('Boton Clicado')
+        }
+
+    function ProjectCode() {
+        // <Redirect to="https://github.com/Christian-Ares/final_react" />
+        console.log('Boton Clicado')
+        }
+
     return (
         <div>
-            <button>Proyecto 3</button>
-            <button>Proyecto 3</button>
+            <Button onClick={ProjectDemo} variant="contained" color="primary">Demo</Button>
+            <Button onClick={ProjectCode} variant="contained" color="primary">Source Code</Button>
+            
         </div>
     )
 }
