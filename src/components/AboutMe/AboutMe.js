54b47b3d017a9ea53bf965477bcd509792a7f223
Christian Ares <christianaresvillan@gmail.com>
2021-02-01 22:21:24 +0100

    Making changes on the components

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Typical from 'react-typical'
 import './Aboutme.css'
 
 export const AboutMe = () => {
     return (
         <div className="aboutMe" id="aboutMe">
             <h1>About Me</h1>
-            <p>Hi! My name's Christian and I'm a Full Stack Developer!</p>
+            <p>Hi! My name's Christian</p>
+                <Typical
+                steps={['Full Stack Developer', 1000, 'Back End Developer', 500, 'Front End Developer', 250]}
+                loop={Infinity}
+                />
+                <section className="sectionText">
+                    <div>
+                        <img src="/images/Christian.jpg" alt="Christian Ares" className="photo" />
+                    </div>
+                    <p>After several years in the hostelry industry, I decided to turn my life around and train as a web developer. I like programming in all its aspects.
+                        I like to learn new things every day, because I believe that learning should never stop.
+                        I love teamwork, working hard and with dedication, leaning on my colleagues to achieve all the goals we set ourselves.
+                        My goal is to work as a Full Stack Developer in a company where I can train and grow, both as a developer and as a person.
+                    </p>
+                </section>
         </div>
     )
 };
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -7,6 +7,12 @@ export const Skills = () => {
         <div className="skills">
             <h1>Skills</h1>
             <h3>Tech Skills</h3>
+            <span class="iconify" data-icon="logos:react" data-inline="false"></span>
+            <span class="iconify" data-icon="logos:html-5" data-inline="false"></span>
+            <span class="iconify" data-icon="vscode-icons:file-type-css" data-inline="false"></span>
+            <span class="iconify" data-icon="logos:sass" data-inline="false"></span>
+            <span class="iconify" data-icon="logos:nodejs" data-inline="false"></span>
+            <span class="iconify" data-icon="vscode-icons:file-type-js-official" data-inline="false"></span>
         </div>
     )
 };
