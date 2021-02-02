650b87781eb0f45a27bebde9ebedfcb49e56cf50
Christian Ares <christianaresvillan@gmail.com>
2021-02-02 21:57:41 +0100

    Working on Tilty Styles

--- src/components/Projects/Proyecto1/Proyecto1.js
+++ src/components/Projects/Proyecto1/Proyecto1.js
@@ -8,9 +8,11 @@ export const Proyecto1 = () => {
 
     return (
         <div className="botones1">
-            <Tilty>
-                <img src="/images/The-Legend-of-Ironhacker.jpg" alt="The Legend of Ironhacker" />
-            </Tilty>
+            {/* <div className="tilty-img"> */}
+                <Tilty style={{width:369, heigth:397}} reset={true}>
+                    <img src="/images/The-Legend-of-Ironhacker.jpg" alt="The Legend of Ironhacker" />
+                </Tilty>
+            {/* </div>     */}
             <div></div>
            <Button variant="contained" color="primary" href="https://christian-ares.github.io/The-Legend-of-Ironhacker/">Demo</Button>
            <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/The-Legend-of-Ironhacker">Source Code</Button>
