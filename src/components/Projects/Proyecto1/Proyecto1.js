5fed4f7dcea0c32585e0c72814877bdfd18efa3b
Christian Ares <christianaresvillan@gmail.com>
2021-02-02 19:41:45 +0100

    Working on images

--- src/components/Projects/Proyecto1/Proyecto1.js
+++ src/components/Projects/Proyecto1/Proyecto1.js
@@ -1,12 +1,16 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import Tilty from 'react-tilty'
+
 
 
 export const Proyecto1 = () => {
 
     return (
         <div className="botones1">
+            
             <img src="/images/The-Legend-of-Ironhacker.jpg" alt="The Legend of Ironhacker" />
+            
             <div></div>
            <Button variant="contained" color="primary" href="https://christian-ares.github.io/The-Legend-of-Ironhacker/">Demo</Button>
            <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/The-Legend-of-Ironhacker">Source Code</Button>
