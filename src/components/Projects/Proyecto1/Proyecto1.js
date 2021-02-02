534e60ff6b1edfaa22aafe67914c3915caeafe7b
Christian Ares <christianaresvillan@gmail.com>
2021-02-02 21:59:38 +0100

    Changes

--- src/components/Projects/Proyecto1/Proyecto1.js
+++ src/components/Projects/Proyecto1/Proyecto1.js
@@ -8,6 +8,7 @@ export const Proyecto1 = () => {
 
     return (
         <div className="botones1">
+                                    {/* Probar a ponerle un padding a la imagen para dejarla a la izquierda, hacer con todas las imagenes */}
             {/* <div className="tilty-img"> */}
                 <Tilty style={{width:369, heigth:397}} reset={true}>
                     <img src="/images/The-Legend-of-Ironhacker.jpg" alt="The Legend of Ironhacker" />
