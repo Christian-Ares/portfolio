a6b27b5a3820d0798a216dee8033b5455cb3096d
Christian Ares <christianaresvillan@gmail.com>
2021-02-02 20:14:20 +0100

    Added Tilty

--- src/components/Projects/Proyecto2/Proyecto2.js
+++ src/components/Projects/Proyecto2/Proyecto2.js
@@ -1,11 +1,14 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import Tilty from "react-tilty";
 
 export const Proyecto2 = () => {
 
     return (
         <div>
-            <img src="/images/MascotApp.png" alt="MascotApp" />
+            <Tilty>
+                <img src="/images/MascotApp.png" alt="MascotApp" />
+            </Tilty>
             <div></div>
             <Button variant="contained" color="primary" href="https://mascotapp-v1.herokuapp.com/">Demo</Button>
             <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/MascotApp">Source Code</Button>
--- src/components/Projects/Proyecto3/Proyecto3.js
+++ src/components/Projects/Proyecto3/Proyecto3.js
@@ -1,11 +1,14 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import Tilty from 'react-tilty'
 
 export const Proyecto3 = () => {
 
     return (
         <div>
-            <img src="/images/Mi-Primer-Paso.png" alt="Mi Primer Paso" />
+            <Tilty>
+                <img src="/images/Mi-Primer-Paso.png" alt="Mi Primer Paso" />
+            </Tilty>
             <div></div>
             <Button variant="contained" color="primary" href="https://escuela-infantil.netlify.app/">Demo</Button>
             <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/final_react">Source Code</Button>
