08eac565bf5e54e41ea55c06b4e009dca7576803
Christian Ares <christianaresvillan@gmail.com>
2021-02-04 21:44:02 +0100

    Installing Sass

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AwesomeButton } from "react-awesome-button";
-import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
-import styles from "morty-theme"
+// import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import styles from "react-awesome-button/src/styles/themes/theme-rickiest"
 
 // Error de Sass, corregir version y poner la 4.0.0
 
@@ -9,7 +9,7 @@ const Button = () => {
     return (
         <AwesomeButton
         size="small"
-        cssModule={AwesomeButtonStyles} 
+        cssModule={styles} 
         type="anchor">
         </AwesomeButton>
     );
