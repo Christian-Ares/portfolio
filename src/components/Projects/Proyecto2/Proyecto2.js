ca1089615526394c63b8032c191e590cb86cc52c
Christian Ares <christianaresvillan@gmail.com>
2021-02-09 20:32:09 +0100

    Links almost done

--- src/components/Projects/Proyecto2/Proyecto2.js
+++ src/components/Projects/Proyecto2/Proyecto2.js
@@ -1,4 +1,3 @@
-import { Button } from '@material-ui/core'
 import React from 'react'
 import Tilty from "react-tilty";
 
@@ -19,8 +18,10 @@ export const Proyecto2 = () => {
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
                     </strong></p>
                         <br/>
-                <Button variant="contained" color="primary" href="https://mascotapp-v1.herokuapp.com/">Demo</Button>
-                <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/MascotApp">Source Code</Button>
+                    <div className="links">
+                        <a href="https://mascotapp-v1.herokuapp.com/" className="projectLink"><span>Demo</span></a>
+                        <a href="https://github.com/Christian-Ares/MascotApp" className="projectLink"><span>Source Code</span></a>
+                    </div>
             </div>
         </div>
     )
--- src/components/Projects/Proyecto3/Proyecto3.js
+++ src/components/Projects/Proyecto3/Proyecto3.js
@@ -1,4 +1,3 @@
-import { Button } from '@material-ui/core'
 import React from 'react'
 import Tilty from 'react-tilty'
 
@@ -11,34 +10,17 @@ export const Proyecto3 = () => {
                     <img src="/images/Mi-Primer-Paso.png" alt="Mi Primer Paso" />
                 </Tilty>
             </div>
-
-        <br/>
-
-            <div className="buttons">
-                <p><strong>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
-                </strong></p>
             <br/>
-                <Button variant="contained" color="primary" href="https://escuela-infantil.netlify.app/">Demo</Button>
-                <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/final_react">Source Code</Button>
+                <div className="buttons">
+                    <p><strong>
+                    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    </strong></p>
+                <br/>
+                <div className="links">
+                    <a href="https://escuela-infantil.netlify.app/" className="projectLink"><span>Demo</span></a>
+                    <a href="https://github.com/Christian-Ares/final_react" className="projectLink"><span>Source Code</span></a>
+                </div>
             </div>
         </div>
     )
-}
-
-<div className="project">
-<div>
-    <Tilty style={{width:639, heigth:397}} reset={true}>
-        <img src="/images/MascotApp.png" alt="MascotApp" />
-    </Tilty>
-</div> 
-
-<br/>   
-
-<div className="buttons">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
-            <br/>
-    <Button variant="contained" color="primary" href="https://mascotapp-v1.herokuapp.com/">Demo</Button>
-    <Button variant="contained" color="primary" href="https://github.com/Christian-Ares/MascotApp">Source Code</Button>
-</div>
-</div>
+};
