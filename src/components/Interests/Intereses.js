e45e8ea431a0b6e5405f6f9c31c9ab5cff3b990a
Christian Ares <christianaresvillan@gmail.com>
2021-03-12 19:30:18 +0100

    Responsive Skills

--- src/components/Interests/Intereses.js
+++ src/components/Interests/Intereses.js
@@ -5,7 +5,7 @@ export const Intereses = () => {
     return (
         <div className="interests" id="interests">
             <h1>Current Interests</h1>
-            <div className="parentSkills">
+            <div className="parentInterests">
                 <span className="iconify span1" data-icon="logos:angular-icon" data-inline="false"></span>
                 <span className="iconify span2" data-icon="logos:typescript-icon" data-inline="false"></span>
                 <span className="iconify span3" data-icon="cib:mysql" data-inline="false"></span>
