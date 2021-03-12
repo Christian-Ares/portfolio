d4e349dd400628d757f7e92895b8c1fdf4dc3f92
Christian Ares <christianaresvillan@gmail.com>
2021-03-12 19:46:06 +0100

    Interests Responsive

--- src/components/Interests/Intereses.js
+++ src/components/Interests/Intereses.js
@@ -6,10 +6,10 @@ export const Intereses = () => {
         <div className="interests" id="interests">
             <h1>Current Interests</h1>
             <div className="parentInterests">
-                <span className="iconify span1" data-icon="logos:angular-icon" data-inline="false"></span>
-                <span className="iconify span2" data-icon="logos:typescript-icon" data-inline="false"></span>
-                <span className="iconify span3" data-icon="cib:mysql" data-inline="false"></span>
-                <span className="iconify span4" data-icon="logos:flutter" data-inline="false"></span>
+                <span className="iconify span1-1" data-icon="logos:angular-icon" data-inline="false"></span>
+                <span className="iconify span2-2" data-icon="logos:typescript-icon" data-inline="false"></span>
+                <span className="iconify span3-3" data-icon="cib:mysql" data-inline="false"></span>
+                <span className="iconify span4-4" data-icon="logos:flutter" data-inline="false"></span>
             </div>
         </div>
     )
